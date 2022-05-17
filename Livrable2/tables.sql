00f5f9799d44201d49631d11ae928b281e43aa2e
Karitham <kar@karitham.dev>
2022-05-17 16:48:46 +0200

    fix sql

--- Livrable2/tables.sql
+++ Livrable2/tables.sql
@@ -1,14 +1,14 @@
 -- Table de fait
 CREATE TABLE IF NOT EXISTS facts (
-	nb_hospitalisations INTEGER,
-	nb_deces INTEGER,
-	nb_consultations INTEGER,
-	satisfaction FLOAT,
-	localisation_id INTEGER,
-	diagnostique_id INTEGER,
-	professionel_id INTEGER,
-	date_id INTEGER,
-	patient_id INTEGER
+ nb_hospitalisations INT,
+ nb_deces INT,
+ nb_consultations INT,
+ satisfaction FLOAT,
+ localisation_id INT,
+ diagnostique_id INT,
+ professionel_id INT,
+ date_id INT,
+ patient_id INT
 )
 COMMENT 'Table de faits'
 ROW FORMAT DELIMITED
@@ -18,10 +18,10 @@ STORED AS TEXTFILE;
 
 -- Dates
 CREATE TABLE IF NOT EXISTS dates (
-	id INTEGER,
-	year INT8,
-	month INT8,
-	day INT16
+ id INT,
+ year INT,
+ month INT,
+ day INT
 )
 ROW FORMAT DELIMITED
 FIELDS TERMINATED BY '\;'
@@ -30,8 +30,8 @@ STORED AS TEXTFILE;
 
 -- Localisations
 CREATE TABLE IF NOT EXISTS localisations (
-	id INTEGER,
-	region VARCHAR
+ id INT,
+ region VARCHAR(256)
 )
 COMMENT 'Table de localisation, ne stoque que les regions'
 ROW FORMAT DELIMITED
@@ -41,9 +41,9 @@ STORED AS TEXTFILE;
 
 -- Diagnostiques
 CREATE TABLE IF NOT EXISTS diagnostiques (
-	id INTEGER,
-	code_diag VARCHAR,
-	diagnostique TEXT
+ id INT,
+ code_diag VARCHAR(256),
+ diagnostique TEXT
 )
 COMMENT 'Table de diagnostiques, stoque un code de diagnostique ainsi que sa definition'
 ROW FORMAT DELIMITED
@@ -54,9 +54,9 @@ STORED AS TEXTFILE;
 
 -- Patients
 CREATE TABLE IF NOT EXISTS patients (
-	id INTEGER,
-	sexe VARCHAR,
-	age INT8
+ id INT,
+ sexe VARCHAR(256),
+ age INT
 )
 COMMENT 'Table de patients, sexe et age'
 ROW FORMAT DELIMITED
@@ -66,12 +66,12 @@ STORED AS TEXTFILE;
 
 -- Professionels
 CREATE TABLE IF NOT EXISTS professionels (
-	id INTEGER,
-	nom VARCHAR,
-	etablissement VARCHAR
+ id INT,
+ nom VARCHAR(256),
+ etablissement VARCHAR(256)
 )
 COMMENT 'Table de pro, stoque le nom et la raison sociale (etablissement)'
 ROW FORMAT DELIMITED
-FIELD TERMINATED BY '\;'
+FIELDS TERMINATED BY '\;'
 LINES TERMINATED BY '\n'
 STORED AS TEXTFILE;
