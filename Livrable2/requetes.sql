7e196950f9b6c79a1e48a5794a97e23f1225f4c3
Karitham <kar@karitham.dev>
2022-05-18 16:13:31 +0200

    fix requetes (ithink)

--- Livrable2/requetes.sql
+++ Livrable2/requetes.sql
@@ -2,48 +2,48 @@
 SELECT avg(faits.nb_consultations)
 FROM faits
 	INNER JOIN dates ON faits.date_id = dates.id
-WHERE faits.date_id % 10 = 0 -- consultation
+WHERE faits.nb_consultations IS NOT NULL
 	AND dates.year = ?
 	AND faits.professionnel_id = ?;
 -- Taux de consultation des patients par rapport à un diagnostic X sur une période de temps Y
 SELECT sum(faits.nb_consultations) / (sum(faits.nb_consultations) over())
 FROM faits
 	INNER JOIN dates ON faits.date_id = dates.id
-WHERE faits.date_id % 10 = 0 -- consultation
+WHERE faits.nb_consultations IS NOT NULL
 	AND faits.diagnostique_id = ?
 	AND dates.year = ?;
 -- Taux global d'hospitalisation des patients dans une période donnée Y
 SELECT sum(faits.nb_hospitalisations) / (sum(faits.nb_hospitalisations) over())
 FROM faits
 	INNER JOIN dates ON faits.date_id = dates.id
-WHERE faits.date_id % 10 = 1 -- hospi
+WHERE faits.nb_hospitalisations IS NOT NULL
 	AND faits.diagnostique_id = ?
 	AND dates.year = ?;
 -- Taux d'hospitalisation des patients par rapport à des diagnostics sur une période donnée
 SELECT sum(faits.nb_hospitalisations) / (sum(faits.nb_hospitalisations) over())
 FROM faits
 	INNER JOIN dates ON faits.date_id = dates.id
-WHERE faits.date_id % 10 = 1 -- hospi
+WHERE faits.nb_hospitalisations IS NOT NULL
 	AND dates.year = ?
 GROUP BY faits.diagnostique_id;
 -- Taux global de satisfaction par région sur l'année 2020
 SELECT faits.satisfaction
 FROM faits
 	INNER JOIN localisations ON faits.localisation_id = localisations.id
-WHERE faits.date_id % 10 = 3
+WHERE faits.satisfaction IS NOT NULL
 GROUP BY faits.localisation_id;
 -- Nombre de décès par localisation (région) et sur l'année 2019
 SELECT sum(faits.nb_deces),
 	faits.localisation_id
 FROM faits
 	INNER JOIN localisations ON faits.localisation_id = localisations.id
-WHERE faits.date_id % 10 = 2
+WHERE faits.nb_deces IS NOT NULL
 GROUP BY faits.localisation_id;
 -- Taux de consultation par professionnel
 SELECT sum(faits.nb_consultations),
 	faits.professionnel_id
 FROM faits
-WHERE faits.date_id % 10 = 0
+WHERE faits.nb_consultations IS NOT NULL
 GROUP BY faits.professionnel_id;
 -- Taux d'hospitalisation/consultation par sexe, par âge
 SELECT sum(faits.nb_consultations),
