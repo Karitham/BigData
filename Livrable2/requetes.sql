6a39ef304a171580d2e5e1925ec0a9bea318c7e5
Karitham <kar@karitham.dev>
2022-05-18 17:35:59 +0200

    fix quelque requetes

--- Livrable2/requetes.sql
+++ Livrable2/requetes.sql
@@ -1,37 +1,40 @@
 -- Taux de consultation des patients dans un établissement X sur une période de temps Y
-SELECT avg(faits.nb_consultations)
+SELECT sum(faits.nb_consultations) / (count(*) over()),
+	faits.professionnel_id
 FROM faits
 	INNER JOIN dates ON faits.date_id = dates.id
 WHERE faits.nb_consultations IS NOT NULL
 	AND dates.year = ?
-	AND faits.professionnel_id = ?;
+GROUP BY faits.professionnel_id;
 -- Taux de consultation des patients par rapport à un diagnostic X sur une période de temps Y
-SELECT sum(faits.nb_consultations) / (sum(faits.nb_consultations) over())
+SELECT sum(faits.nb_consultations) / (count(*) over()),
+	diagnostique_id
 FROM faits
 	INNER JOIN dates ON faits.date_id = dates.id
 WHERE faits.nb_consultations IS NOT NULL
-	AND faits.diagnostique_id = ?
-	AND dates.year = ?;
+	AND dates.year = ?
+GROUP BY diagnostique_id;
 -- Taux global d'hospitalisation des patients dans une période donnée Y
-SELECT sum(faits.nb_hospitalisations) / (sum(faits.nb_hospitalisations) over())
+SELECT sum(faits.nb_hospitalisations) / (count(*) over()),
+	dates.year -- TODO FIXME WTF CA MARCHE PAS
 FROM faits
 	INNER JOIN dates ON faits.date_id = dates.id
 WHERE faits.nb_hospitalisations IS NOT NULL
-	AND faits.diagnostique_id = ?
-	AND dates.year = ?;
+GROUP BY dates.year;
 -- Taux d'hospitalisation des patients par rapport à des diagnostics sur une période donnée
-SELECT sum(faits.nb_hospitalisations) / (sum(faits.nb_hospitalisations) over())
+SELECT sum(faits.nb_hospitalisations) / (sum(faits.nb_hospitalisations) over()),
+	faits.diagnostique_id -- TODO FIX 
 FROM faits
 	INNER JOIN dates ON faits.date_id = dates.id
 WHERE faits.nb_hospitalisations IS NOT NULL
 	AND dates.year = ?
 GROUP BY faits.diagnostique_id;
 -- Taux global de satisfaction par région sur l'année 2020
-SELECT faits.satisfaction
+SELECT faits.satisfaction,
+	localisations.id
 FROM faits
 	INNER JOIN localisations ON faits.localisation_id = localisations.id
-WHERE faits.satisfaction IS NOT NULL
-GROUP BY faits.localisation_id;
+WHERE faits.satisfaction IS NOT NULL;
 -- Nombre de décès par localisation (région) et sur l'année 2019
 SELECT sum(faits.nb_deces),
 	faits.localisation_id
@@ -40,13 +43,13 @@ FROM faits
 WHERE faits.nb_deces IS NOT NULL
 GROUP BY faits.localisation_id;
 -- Taux de consultation par professionnel
-SELECT sum(faits.nb_consultations),
+SELECT sum(faits.nb_consultations) / (count(*) over()),
 	faits.professionnel_id
 FROM faits
 WHERE faits.nb_consultations IS NOT NULL
 GROUP BY faits.professionnel_id;
 -- Taux d'hospitalisation/consultation par sexe, par âge
-SELECT sum(faits.nb_consultations),
+SELECT sum(faits.nb_consultations) + sum(faits.nb_hospitalisations),
 	patients.sexe,
 	patients.age
 FROM faits
